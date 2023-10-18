cfea002d8662c1754ff99642122d1f7ae0824b6e
Nikita Averochkin <dota.nikita@inbox.ru>
2023-10-18 17:13:45 +0300

    little refactor

--- src/shared/ui/title/index.tsx
+++ src/shared/ui/title/index.tsx
@@ -28,25 +28,15 @@ const Title: FC<ITitle & HTMLAttributes<HTMLHeadElement>> = ({
         ...restBasicProps
     });
 
-    switch (titleType) {
-        case titleTypes.h6: {
-            return <h6 {...getTitleProps(styles.titleH6)}>{text}</h6>;
-        }
-        case titleTypes.h5: {
-            return <h5 {...getTitleProps(styles.titleH5)}>{text}</h5>;
-        }
-        case titleTypes.h4: {
-            return <h4 {...getTitleProps(styles.titleH4)}>{text}</h4>;
-        }
-        case titleTypes.h3: {
-            return <h3 {...getTitleProps(styles.titleH3)}>{text}</h3>;
-        }
-        case titleTypes.h2: {
-            return <h2 {...getTitleProps(styles.titleH2)}>{text}</h2>;
-        }
-        default: {
-            return <h1 {...getTitleProps(styles.titleH1)}>{text}</h1>;
-        }
-    }
+    const titleObj = {
+        [titleTypes.h1]: <h1 {...getTitleProps(styles.titleH1)}>{text}</h1>,
+        [titleTypes.h2]: <h2 {...getTitleProps(styles.titleH2)}>{text}</h2>,
+        [titleTypes.h3]: <h3 {...getTitleProps(styles.titleH3)}>{text}</h3>,
+        [titleTypes.h4]: <h4 {...getTitleProps(styles.titleH4)}>{text}</h4>,
+        [titleTypes.h5]: <h5 {...getTitleProps(styles.titleH5)}>{text}</h5>,
+        [titleTypes.h6]: <h6 {...getTitleProps(styles.titleH6)}>{text}</h6>
+    };
+
+    return titleObj[titleType ?? titleTypes.h1];
 };
 export default Title;
